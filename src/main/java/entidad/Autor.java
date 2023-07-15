12150cafd1f56dfd5d3dc60972368ca53c507acc
YasarChavez <tneniitoo@gmail.com>
2023-07-15 01:58:56 -0300

    Updates

--- src/main/java/entidad/Autor.java
+++ src/main/java/entidad/Autor.java
@@ -1,11 +1,11 @@
 package entidad;
 
+
 import javax.persistence.*;
-import java.io.Serializable;
 
 @Entity
-//@Table (name = "autor")
-public class Autor implements Serializable {
+@Table (name = "autor")
+public class Autor{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
